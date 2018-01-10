52e18c69aa9a3b05203024ea264417dfb88f145c
Scott McKenzie <scott@noizyland.net>
2018-01-10 19:54:45 +1100

    Update python install requirements

--- setup.py
+++ setup.py
@@ -3,12 +3,15 @@ from setuptools import setup
 setup(
     name='holman',
     packages=['holman'],
-    install_requires=['gatt>=0.2.7'],
-    version='0.0.3',
+    install_requires=[
+        'dbus-python==1.2.4',
+        'gatt>=0.2.7'
+    ],
+    version='0.0.4',
     description='Holman SDK for Python on Linux',
     keywords='holman',
     url='https://github.com/scottmckenzie/holman-linux-python',
-    download_url='https://github.com/scottmckenzie/holman-linux-python/archive/0.0.3.tar.gz',
+    download_url='https://github.com/scottmckenzie/holman-linux-python/archive/0.0.4.tar.gz',
     author='Scott McKenzie',
     author_email='developers@noizyland.net',
     license='MIT',
